2ff7676e9f46b116724ca40a493af8c58b16a1f2
Swapnil Davare <swapnil.davare@innofied.com>
2021-06-03 15:49:58 +0530

    example added

---
+++ example_customswitch/android/app/src/main/kotlin/com/example/example_customswitch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_customswitch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example_customswitch/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:custom_switch_lib/custom_switch_lib.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Custom Switch Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Custom Switch Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String status = "In-Active";
+  bool isEnable = false;
+  bool isActiveValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$status',
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CustomSwitch(
+              activeColor: Colors.blue,
+              inactiveColor: Colors.grey,
+              value: isActiveValue,
+              widht: 90,
+              height: 40,
+              circle_widht: 42,
+              circle_height: 42,
+              circle_radious: 42,
+              enabled: isEnable,
+              onChanged: (bool val) {
+                setState(() {
+                  if (val) {
+                    status = "In-Active";
+                    isEnable = !val;
+                    isActiveValue = !val;
+                  } else {
+                    status = "Active";
+                    isEnable = !val;
+                    isActiveValue = !val;
+                  }
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
